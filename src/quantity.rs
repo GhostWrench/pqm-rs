9beda982687573ded55300d0c24a5587d2bd1e16
Daniel <daniel.kuntz@ghostwrench.net>
2020-07-01 08:29:40 -0600

    Add initial quantity type and basic methods

---
+++ src/quantity.rs
@@ -0,0 +1,73 @@
+const NUM_DIMENSION_TYPES: usize = 7;
+
+pub struct Quantity {
+    magnitude: f64,
+    dimensions: [i32; NUM_DIMENSION_TYPES],    
+}
+
+impl Quantity {
+
+    /// Create a new Quantity
+    /// 
+    /// # Arguments
+    /// 
+    /// * `magnitude` - Raw magnitude of the quantity
+    /// * `dimensions` - Array of values that define the "dimensionality" of the quantity
+    /// 
+    pub fn new(magnitude: f64, dimensions: [i32; NUM_DIMENSION_TYPES]) -> Quantity {
+        return Quantity {
+            magnitude: magnitude,
+            dimensions: dimensions,
+        }
+    }
+
+    /// Check if two quantities have the same dimensionality
+    /// 
+    /// # Arguments
+    /// 
+    /// * `other` - Other quantity to compare this quantity to
+    /// 
+    /// # Return
+    /// 
+    /// When the return value is:
+    ///   * true: The quantities have the same dimensionality
+    ///   * false: The quantities do not have the same dimensionality
+    ///
+    pub fn same_dimensionality(&self, other: &Quantity) -> bool {
+        //let mut return_val: bool = true;
+        for dim_idx in 0..(NUM_DIMENSION_TYPES-1) {
+            if self.dimensions[dim_idx] != other.dimensions[dim_idx] {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// Add two quantities together
+    /// 
+    /// # Arguments
+    /// 
+    /// * `other` - Other quantity to add to this quantity
+    /// 
+    /// # Return
+    /// 
+    /// Returns a Result enum with the added quantity if the operation is allowed
+    /// 
+    pub fn add(&self, other: &Quantity) -> Quantity {
+        // Check for user input error: TODO Convert to Result instead of panic!
+        if !self.same_dimensionality(other) {
+            panic!("Cannot add two quantities with unlike types!");
+        };
+        return Quantity {
+            magnitude: self.magnitude + other.magnitude,
+            dimensions: self.dimensions.clone()
+        }
+    }
+
+    pub fn eq(&self, other: &Quantity) -> bool {
+        if !self.same_dimensionality(other) || (self.magnitude != other.magnitude) {
+            return false;
+        };
+        return true;
+    }
+}
---
+++ tests/tests.rs
@@ -0,0 +1,10 @@
+use pqm_rs::quantity::Quantity;
+
+#[test]
+fn add_quantities() {
+    let q1 = Quantity::new(1.0, [1,0,0,0,0,0,0]);
+    let q2 = Quantity::new(1.0, [1,0,0,0,0,0,0]);
+    let expected = Quantity::new(2.0, [1,0,0,0,0,0,0]);
+    let result = q1.add(&q2);
+    assert!(result.eq(&expected));
+}
